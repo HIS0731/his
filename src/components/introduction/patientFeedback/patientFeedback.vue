95ee6671c3a3f75d514a290afb7d4ae6607350a2
dyingying <1781443968@qq.com>
2017-08-09 13:56:01 +0800

    测试报告提交

--- src/components/introduction/patientFeedback/patientFeedback.vue
+++ src/components/introduction/patientFeedback/patientFeedback.vue
@@ -10,145 +10,95 @@
             </el-form-item>
           </el-col>
         </el-form-item>
-        <el-form-item label="住院日期" required>
+        <el-form-item label="就诊日期" required>
            <el-col :span="11">
             <el-form-item prop="date2">
-              <el-date-picker type="date" placeholder="选择住院日期" v-model="ruleForm.date2" style="width: 150px;"></el-date-picker>
+              <el-date-picker type="date" placeholder="选择就诊日期" v-model="ruleForm.date2" style="width: 150px;"></el-date-picker>
             </el-form-item>
           </el-col>
+        </el-form-item>      
+      </el-form>
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item label="问题一：您对医院的总的印象如何？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer1" :max="1">
+            <el-checkbox label="checkbox1" name="answer">满意</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">基本满意</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">不满意</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题二：您对医院工作人员的服务态度总体上是否满意？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer2" :max="1">
+            <el-checkbox label="checkbox1" name="answer">满意</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">基本满意</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">不满意</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题三：您对医院提供的诊疗技术是否满意？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer3" :max="1">
+            <el-checkbox label="checkbox1" name="answer">满意</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">基本满意</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">不满意</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题四：您对医院的就诊环境是否满意？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer4" :max="1">
+            <el-checkbox label="checkbox1" name="answer">满意</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">基本满意</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">不满意</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题五：您对医院的服务设施是否满意？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer5" :max="1">
+            <el-checkbox label="checkbox1" name="answer">满意</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">基本满意</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">不满意</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题六：您认为医院是否存在违规收费行为？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer6" :max="1">
+            <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题七：您或您的家属在予以就诊或住院过程中，是否宴请过该医院医务人员？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer7" :max="1">
+            <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题八：您认为医院是尊重、关爱患者，做到主动、热情、周到、文明服务？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer8" :max="1">
+           <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题九：您询问医务人员或有意见向医院投诉，医院工作人员是否热情接待，耐心解释？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer9" :max="1">
+            <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题十：您或您的家属在医院就诊时，是否被医生推诿、拒绝过？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer10" :max="1">
+            <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="问题十一：您认为医院是否做到“一切以病人为中心”？" prop="answer" required>
+          <el-checkbox-group v-model="ruleForm.answer11" :max="1">
+            <el-checkbox label="checkbox1" name="answer">是</el-checkbox>
+            <el-checkbox label="checkbox2" name="answer">否</el-checkbox>
+            <el-checkbox label="checkbox3" name="answer">其他</el-checkbox>
+          </el-checkbox-group>
         </el-form-item>
       </el-form>
-          <div class="checkbox">
-          <span class="question">问题一：您对医院的总的印象如何？</span>
-            <label v-for="item in list1">
-            <input
-            type="checkbox"
-            v-model="toggle1"
-            name="list1"
-            :true-value="item"
-            :false-value="b">{{item.number1}}
-            </label>
-          </div>
 
-          <div class="checkbox">
-          <span class="question">问题二：您对医院工作人员的服务态度总体上是否满意？</span>
-            <label v-for="item in list2">
-            <input
-            type="checkbox"
-            v-model="toggle2"
-            name="list2"
-            :true-value="item"
-            :false-value="b">{{item.number2}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题三：您对医院提供的诊疗技术是否满意？</span>
-            <label v-for="item in list3">
-            <input
-            type="checkbox"
-            v-model="toggle3"
-            name="list3"
-            :true-value="item"
-            :false-value="b">{{item.number3}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题四：您对医院的就诊环境是否满意？</span>
-            <label v-for="item in list4">
-            <input
-            type="checkbox"
-            v-model="toggle4"
-            name="list4"
-            :true-value="item"
-            :false-value="b">{{item.number4}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题五：您对医院的服务设施是否满意？</span>
-            <label v-for="item in list5">
-            <input
-            type="checkbox"
-            v-model="toggle5"
-            name="list5"
-            :true-value="item"
-            :false-value="b">{{item.number5}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题六：您认为医院是否存在违规收费行为？</span>
-            <label v-for="item in list6">
-            <input
-            type="checkbox"
-            v-model="toggle6"
-            name="list6"
-            :true-value="item"
-            :false-value="b">{{item.number6}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题七：您或您的家属在予以就诊或住院过程中，是否宴请过该医院医务人员？</span>
-            <label v-for="item in list7">
-            <input
-            type="checkbox"
-            v-model="toggle7"
-            name="list7"
-            :true-value="item"
-            :false-value="b">{{item.number7}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题八：您认为医院是尊重、关爱患者，做到主动、热情、周到、文明服务？</span>
-            <label v-for="item in list8">
-            <input
-            type="checkbox"
-            v-model="toggle8"
-            name="list8"
-            :true-value="item"
-            :false-value="b">{{item.number8}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题九：您询问医务人员或有意见向医院投诉，医院工作人员是否热情接待，耐心解释？</span>
-            <label v-for="item in list9">
-            <input
-            type="checkbox"
-            v-model="toggle9"
-            name="list9"
-            :true-value="item"
-            :false-value="b">{{item.number9}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题十：您或您的家属在医院就诊时，是否被医生推诿、拒绝过？</span>
-            <label v-for="item in list10">
-            <input
-            type="checkbox"
-            v-model="toggle10"
-            name="list10"
-            :true-value="item"
-            :false-value="b">{{item.number10}}
-            </label>
-          </div>
-
-          <div class="checkbox">
-          <span class="question">问题十一：您认为医院是否做到“一切以病人为中心”？</span>
-            <label v-for="item in list11">
-            <input
-            type="checkbox"
-            v-model="toggle11"
-            name="list11"
-            :true-value="item"
-            :false-value="b">{{item.number11}}
-            </label>
-          </div>
+      
       <el-form>
           <el-form-item>
             <el-button type="primary" @click="onSubmit('ruleForm')">提交</el-button>
@@ -163,76 +113,20 @@
   export default {
     data () {
       return {
-        list1: [
-          {number1: '满意'},
-          {number1: '基本满意'},
-          {number1: '不满意'}
-        ],
-        list2: [
-          {number2: '满意'},
-          {number2: '基本满意'},
-          {number2: '不满意'}
-        ],
-        list3: [
-          {number3: '满意'},
-          {number3: '基本满意'},
-          {number3: '不满意'}
-        ],
-        list4: [
-          {number4: '满意'},
-          {number4: '基本满意'},
-          {number4: '不满意'}
-        ],
-        list5: [
-          {number5: '满意'},
-          {number5: '基本满意'},
-          {number5: '不满意'}
-        ],
-        list6: [
-          {number6: '是'},
-          {number6: '否'},
-          {number6: '其他'}
-        ],
-        list7: [
-          {number7: '是'},
-          {number7: '否'},
-          {number7: '其他'}
-        ],
-        list8: [
-          {number8: '是'},
-          {number8: '否'},
-          {number8: '其他'}
-        ],
-        list9: [
-          {number9: '是'},
-          {number9: '否'},
-          {number9: '其他'}
-        ],
-        list10: [
-          {number10: '是'},
-          {number10: '否'},
-          {number10: '其他'}
-        ],
-        list11: [
-          {number11: '是'},
-          {number11: '否'},
-          {number11: '其他'}
-        ],
-        toggle1: {number1: ''},
-        toggle2: {number2: ''},
-        toggle3: {number3: ''},
-        toggle4: {number4: ''},
-        toggle5: {number5: ''},
-        toggle6: {number6: ''},
-        toggle7: {number7: ''},
-        toggle8: {number8: ''},
-        toggle9: {number9: ''},
-        toggle10: {number10: ''},
-        toggle11: {number11: ''},
-        b: {},
         ruleForm: {
           date1: '',
-          date2: ''
+          date2: '',
+          answer1: [],
+          answer2: [],
+          answer3: [],
+          answer4: [],
+          answer5: [],
+          answer6: [],
+          answer7: [],
+          answer8: [],
+          answer9: [],
+          answer10: [],
+          answer11: []
         },
         rules: {
           date1: [
@@ -240,6 +134,39 @@
           ],
           date2: [
             { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+          ],
+          answer1: [
+            { required: true, message: '  请只选择一个', trigger: 'blur' }
+          ],
+          answer2: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer3: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer4: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer5: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer6: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer7: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer8: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer9: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer10: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
+          ],
+          answer11: [
+            { required: true, message: '  请只选择一个', trigger: 'change' }
           ]
         }
       };
@@ -274,8 +201,8 @@
         let day2 = new Date(oldDate2).getDate();
         let dateFormat1 = year1 + '-' + month1 + '-' + day1;
         let dateFormat2 = year2 + '-' + month2 + '-' + day2;
-        console.log(dateFormat1);
-        console.log(dateFormat2);
+        console.log('调查日期：', dateFormat1);
+        console.log('就诊日期：', dateFormat2);
       }
     }
   };
@@ -288,8 +215,8 @@
   .patientFeedback .form
      margin-top:80px
   .patientFeedback
-    height:1000px
-    width:1000px
+    height:850px
+    width:900px
     margin:auto
     border:1px solid black
   .patientFeedback .checkbox
